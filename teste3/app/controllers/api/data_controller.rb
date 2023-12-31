55a24e17fe5d89f44d32129b49b9759e0ef7341e
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-12-31 15:54:27 -0300

    refactor: implementação do codigo na função

--- teste3/app/controllers/api/data_controller.rb
+++ teste3/app/controllers/api/data_controller.rb
@@ -1,23 +1,20 @@
+# app/controllers/api/data_controller.rb
 class Api::DataController < ApplicationController
-    def show
-      input = [
-        { name: 'Maria Neusa de Aparecida', cpf: '97905796671', state: 'Sao Paulo', value: '1234' },
-        { name: 'Ricardo Fontes', cpf: '44010762900', state: 'Rio Grande do Sul', value: '567' }
-      ]
-  
-      render json: { result: solucao(input) }
+    def index
+      people = Person.all
+      render json: { result: solucao(people) }
     end
   
     private
   
-    def solucao(arg)
+    def solucao(people)
       result = ""
   
-      arg.each do |item|
-        name = item[:name][0, 11].ljust(11)
-        cpf = item[:cpf][0, 11].ljust(11)
-        state = item[:state][0, 11].ljust(11)
-        value = item[:value][0, 11].ljust(11)
+      people.each do |person|
+        name = person.name[0, 11].ljust(11)
+        cpf = person.cpf[0, 11].ljust(11)
+        state = person.state[0, 11].ljust(11)
+        value = person.value[0, 11].ljust(11)
   
         result += "#{name}#{cpf}#{state}#{value}\n"
       end
