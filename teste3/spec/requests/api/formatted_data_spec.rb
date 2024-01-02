b7da219f1c22b11a2000ae5028cb906e4ebf134c
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-01-02 09:38:01 -0300

    refactor: implementaçã de melhorias no codigo na função

--- teste3/spec/requests/api/formatted_data_spec.rb
+++ teste3/spec/requests/api/formatted_data_spec.rb
@@ -17,13 +17,11 @@ RSpec.describe Api::FormattedDataController, type: :request do
       json_response = JSON.parse(response.body)
       formatted_data = json_response['result']
 
-      # Ajuste para aceitar espaçamentos flexíveis
       expected_output = <<~EXPECTED
         0009790579667100001234Sao Paulo     
         0004401076290000000567Rio Grande do
       EXPECTED
 
-      # Remova todos os espaços em branco (incluindo espaços, guias e quebras de linha) e compare
       expect(formatted_data.gsub(/\s+/, '')).to eq(expected_output.gsub(/\s+/, ''))
     end
   end
